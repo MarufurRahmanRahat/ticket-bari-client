9e891eea95fa5e7741db7d29c55557c47db9b42f
MarufurRahmanRahat <marufrahat88@gmail.com>
2025-12-07 23:56:51 +0600

    created the password validation code

---
+++ src/components/utils/passwordValidation.js
@@ -0,0 +1,20 @@
+export const validatePassword = (password) => {
+  const errors = [];
+
+  if (password.length < 6) {
+    errors.push("Password must be at least 6 characters");
+  }
+
+  if (!/[A-Z]/.test(password)) {
+    errors.push("Password must contain at least one uppercase letter");
+  }
+
+  if (!/[a-z]/.test(password)) {
+    errors.push("Password must contain at least one lowercase letter");
+  }
+
+  return {
+    isValid: errors.length === 0,
+    errors,
+  };
+};
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import MainLayout from "../layouts/MainLayout";
 
 const router = createBrowserRouter([
   {
