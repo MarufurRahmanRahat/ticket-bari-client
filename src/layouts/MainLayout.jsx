fc46be8d58a92d3202694a39889e0666a0a574f3
MarufurRahmanRahat <marufrahat88@gmail.com>
2025-12-08 00:12:02 +0600

    updated main layout and installed lucide react

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -7,8 +7,10 @@ const MainLayout = () => {
     return (
         <div className='flex flex-col min-h-screen'>
             <Navbar/>
-            <div className='max-w-screen-2xl w-full flex-1'>
-                <Outlet />
+            <div className='flex-1 w-full'>
+                <div className='max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8'>
+                    <Outlet />
+                </div>
             </div>
             <Footer/>
         </div>
